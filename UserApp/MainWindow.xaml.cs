e695955ecbcef2b5c51871bfe74a9522ab73d4ed
unistd.write@gmail.com <unistd.write@gmail.com>
2022-12-24 14:01:16 +0300

    Перед сбросом

--- UserApp/MainWindow.xaml.cs
+++ UserApp/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserApp;
+
+
 
 namespace UserApp
 {
@@ -20,9 +23,69 @@ namespace UserApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        ApplicationContext db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new ApplicationContext();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text;
+            string email = textBoxEmail.Text;
+            string pass = passwordBox.Password;
+            string passTry = passwordBoxTry.Password;
+
+            if (email.Length < 5 | !email.Contains('@') | !email.Contains('.'))
+            {
+                textBoxEmail.ToolTip = "Не корректный ввод поля.";
+                textBoxEmail.Background = Brushes.LightPink;
+            }
+            else
+            {
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+            }
+
+            if (login.Length < 3)
+            {
+                textBoxLogin.ToolTip = "Не корректный ввод поля.";
+                textBoxLogin.Background = Brushes.LightPink;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+            }
+            if (pass.Length < 5)
+            {
+                passwordBox.ToolTip = "Не корректный ввод поля.";
+                passwordBox.Background = Brushes.LightPink;
+            }
+            else
+            {
+                passwordBox.ToolTip = "";
+                passwordBox.Background = Brushes.Transparent;
+            }
+            if (pass != passTry)
+            {
+                passwordBoxTry.ToolTip = "Поля не совпадют.";
+                passwordBoxTry.Background = Brushes.LightPink;
+            }
+            else
+            {
+                passwordBoxTry.ToolTip = "";
+                passwordBoxTry.Background = Brushes.Transparent;
+            }
+            if ((string)passwordBoxTry.ToolTip == "" && (string)passwordBox.ToolTip == "" && (string)textBoxLogin.ToolTip == "" 
+                    && (string)textBoxEmail.ToolTip == "")
+            {
+                MessageBox.Show("Всё хороошо!");
+                User user = new User(login, pass, email);
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
         }
     }
 }
---
+++ UserApp/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp
+{
+    class User
+    {
+        public int id { get; set; }
+
+        private string login, pass, email;
+
+        public string Login
+        {
+            get { return login; }
+            set { this.login = value; }
+        }
+
+        public string Pass
+        {
+            get { return pass; }
+            set { this.pass = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { this.email = value; }
+        }
+
+        public User() {}
+
+        public User(string login, string pass, string email)
+        {
+            this.login = login;
+            this.pass = pass;
+            this.email = email;
+        }
+    }
+}
